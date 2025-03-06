2f9b28902d4d9e963c7cd091d13dcfeaa27aaf3d
AlexTimoncini <alextimonci@gmail.com>
2025-03-06 01:26:25 +0100

    project init

---
+++ js/main.js
@@ -0,0 +1,28 @@
+//parallax
+const hasMouse = navigator.maxTouchPoints === 0;
+if (hasMouse) {
+    document.addEventListener("mousemove", parallax);
+} else {
+    console.log("Parallax disabled: No mouse detected");
+}
+function parallax(event) {
+    this.querySelectorAll(".parallax").forEach((shift) => {
+        const position = shift.dataset.parallax
+        const x = (window.innerWidth - event.pageX * position) / 90
+        const y = (window.innerHeight - event.pageY * position) / 90
+        shift.style.transform = `translateX(${x}px) translateY(${y}px)`
+    })
+}
+window.addEventListener('scroll', fadeIn );
+function fadeIn() {
+    let elementsArray = document.querySelectorAll(".scroll-fade-in");
+    for (let i = 0; i < elementsArray.length; i++) {
+        let elem = elementsArray[i]
+        let distInView = elem.getBoundingClientRect().top - window.innerHeight + 40;
+        if (distInView < 0) {
+            elem.classList.add("visible");
+        } else {
+            elem.classList.remove("visible");
+        }
+    }
+}
