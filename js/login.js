04c34e5c2f0cff9276b7ae718a1a901dbbb86fbd
Poratile_Alex <>
2025-03-17 13:04:57 +0100

    upload

--- js/login.js
+++ js/login.js
@@ -1,5 +1,45 @@
-init()    
+init()
 function init() {
 
+    document.getElementById("un").addEventListener("blur", async function(){
+        let username = document.getElementById("un").value.trim()
+        if(username.length < 2 || username.length > 20){
+            err("un", "Invalid username")
+        } else {
+            d_err("un")
+        }
+    })
+    document.getElementById("pw").addEventListener("blur", async function(){
+        let password = document.getElementById("pw").value.trim()
+        if(!password || password.length > 20){
+            err("pw", "Invalid password")
+        } else {
+            d_err("pw")
+        }
+    })
+    document.getElementById("login-form").addEventListener("submit", function(evt){
+        evt.preventDefault()
+        if(document.querySelectorAll("#login input.error").length > 0){
+            return false
+        }
+        fetch(`${api}auth`, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ un: document.getElementById("un").value.trim(), pw: document.getElementById("pw").value.trim() })
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.result) {
+                    sessionStorage.setItem("token", data.token)
+                    top.location.href = `${domain}/#/dashboard`
+                } else {
+                    alert(data.msg)
+                }
+                return false;
+            })
+            .catch(err => alert("Server error, try again alter."));
+
+        return false
+    })
 
 }
