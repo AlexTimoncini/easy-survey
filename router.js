878d55494021d2572ea78d2461da5f3efd246e84
Poratile_Alex <>
2025-03-17 18:09:35 +0100

    upload

--- router.js
+++ router.js
@@ -44,7 +44,9 @@ router.get('/dashboard', function(){
                     stopLoading()
                 } else {
                     sessionStorage.clear()
-                    top.location.href = `${domain}/#/login`
+                    //top.location.href = `${domain}/#/login` TODO
+                    stopLoading()
+
                 }
             })
         })
@@ -58,11 +60,17 @@ async function buildPage(mainHTML, css, scriptList, isAuth=false){
     removeOldStyles()
     loadCss();
     if (!document.getElementById("loader"))await loader()
+
+    //NO AUTH
     if (!document.getElementById("navbar") && !isAuth)await navbar()
     if (!document.getElementById("footer") && !isAuth)await footer()
+    if(!isAuth && document.getElementById("navbar-auth")) document.getElementById("navbar-auth").remove()
+
+    //AUTH
     if(isAuth && document.getElementById("navbar")) document.getElementById("navbar").remove()
     if(isAuth && document.getElementById("footer")) document.getElementById("footer").remove()
     if(isAuth && !document.getElementById("navbar-auth")) await navbarAuth()
+
     await main()
     await scripts()
 
@@ -178,6 +186,6 @@ function validateToken() {
         .catch(err => {
             console.error(err)
             alert("Server error, try again later")
-            return false;
+            return true; //TODO BACK TO FALSE
         });
 }
