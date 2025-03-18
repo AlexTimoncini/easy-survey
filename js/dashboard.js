a88739f85f64daf4223cdd193672894e5993e773
Poratile_Alex <>
2025-03-18 17:55:26 +0100

    upload

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,5 +1,11 @@
 init()
 async function init() {
-    console.log("dashboard")
-
+    let un = sessionStorage.getItem("username")
+    if(un){
+        document.getElementById("pic").innerHTML = un[0]+un[un.length-1]
+        document.getElementById("un").innerHTML = un
+    } else {
+        sessionStorage.clear()
+        top.location.reload()
+    }
 }
