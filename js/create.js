747d365ee2f93c869f5e90860d2658b68443dccd
Poratile_Alex <>
2025-03-18 17:57:44 +0100

    upload

--- js/create.js
+++ js/create.js
@@ -1,4 +1,11 @@
 init()
-function init() {
-    console.log("create")
+async function init() {
+    let un = sessionStorage.getItem("username")
+    if(un){
+        document.getElementById("pic").innerHTML = un[0]+un[un.length-1]
+        document.getElementById("un").innerHTML = un
+    } else {
+        sessionStorage.clear()
+        top.location.reload()
+    }
 }
