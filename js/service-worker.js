f553e443edadcf9b8a352644511aab9a68fe2f1d
Poratile_Alex <>
2025-03-19 12:08:02 +0100

    upload

--- js/service-worker.js
+++ js/service-worker.js
@@ -7,21 +7,23 @@ self.addEventListener("install", function(event) {
                 '/easy-survey/assets/css/loader.css',
                 '/easy-survey/assets/css/footer.css',
                 '/easy-survey/index.html'
-            ]);
+            ])
         })
-    );
-});
+    )
+})
 
 self.addEventListener("fetch", function(event) {
     event.respondWith(
-        caches.open("pwa").then(function(cache) {
-            return cache.match(event.request).then(function(response) {
-                cache.addAll([event.request.url]);
-                if (response) {
-                    return response;
-                }
-                return fetch(event.request);
-            });
+        caches.match(event.request).then(function(response) {
+            if (response) {
+                return response
+            }
+            return fetch(event.request).then(function(networkResponse) {
+                return caches.open("pwa").then(function(cache) {
+                    cache.put(event.request, networkResponse.clone())
+                    return networkResponse
+                })
+            })
         })
-    );
-});
+    )
+})
