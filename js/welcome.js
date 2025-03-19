239b604b9147eaecf4c68f32d81020d3e5895f47
Poratile_Alex <>
2025-03-19 11:52:56 +0100

    upload

--- js/welcome.js
+++ js/welcome.js
@@ -1,4 +1,11 @@
 init()
 function init() {
-    
+    let installEvent = null;
+    let installButton = document.getElementById("install")
+    document.getElementById("install").style.display = "none"
+    if (localStorage["pwa-enabled"]) {
+        startPwa();
+    } else {
+        startPwa(true);
+    }
 }
