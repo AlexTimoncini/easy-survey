45c1f2282e4e87a92207aab7a88c64074c9c53c9
Poratile_Alex <>
2025-03-19 11:55:28 +0100

    upload

--- js/pwa.js
+++ js/pwa.js
@@ -1,61 +1,58 @@
 function startPwa(firstStart) {
-    localStorage["pwa-enabled"] = true;
-
+    let installEvent = null
+    let installButton = document.getElementById("install")
+    localStorage["pwa-enabled"] = true
     if (firstStart) {
-        location.reload();
+        location.reload()
     }
-
     window.addEventListener("load", () => {
         navigator.serviceWorker.register("/easy-survey/js/service-worker.js", {scope: '/easy-survey/js/'})
             .then(registration => {
-                console.log("Service Worker is registered", registration);
+                console.log("Service Worker is registered", registration)
             })
             .catch(err => {
-                console.error("Registration failed:", err);
-            });
-    });
-
+                console.error("Registration failed:", err)
+            })
+    })
     window.addEventListener("beforeinstallprompt", (e) => {
-        e.preventDefault();
-        console.log("Ready to install...");
-        installEvent = e;
-        document.getElementById("install").style.display = "initial";
+        e.preventDefault()
+        console.log("Ready to install...")
+        installEvent = e
+        document.getElementById("install").style.display = "initial"
     });
-
-    setTimeout(cacheAll, 500);
-
+    setTimeout(cacheAll, 500)
     function cacheAll() {
         caches.open("pwa").then(function(cache) {
-            let linksFound = [];
+            let linksFound = []
             document.querySelectorAll("a").forEach(function(el) {
-                linksFound.push(el.href);
-            });
-            cache.addAll(linksFound);
-            let jsFound = [];
+                linksFound.push(el.href)
+            })
+            cache.addAll(linksFound)
+            let jsFound = []
             document.querySelectorAll("script").forEach(function(el) {
-                linksFound.push(el.src);
-            });
-            cache.addAll(jsFound);
-            let cssFound = [];
+                linksFound.push(el.src)
+            })
+            cache.addAll(jsFound)
+            let cssFound = []
             document.querySelectorAll("link[rel='stylesheet']").forEach(function(el) {
-                linksFound.push(el.href);
-            });
-            cache.addAll(cssFound);
-            let imgFound = [];
+                linksFound.push(el.href)
+            })
+            cache.addAll(cssFound)
+            let imgFound = []
             document.querySelectorAll("img").forEach(function(el) {
-                linksFound.push(el.src);
+                linksFound.push(el.src)
             });
-            cache.addAll(imgFound);
+            cache.addAll(imgFound)
         });
     }
 
     if (installButton) {
         installButton.addEventListener("click", function() {
             if (installEvent) {
-                installEvent.prompt();
+                installEvent.prompt()
             } else {
-                alert("Automatic installer of PWAs not supported on this device, please download it manually from Chrome/Safari menu");
+                alert("Automatic installer of PWAs not supported on this device, please download it manually from Chrome/Safari menu")
             }
-        });
+        })
     }
 }
--- js/welcome.js
+++ js/welcome.js
@@ -1,7 +1,5 @@
 init()
 function init() {
-    let installEvent = null;
-    let installButton = document.getElementById("install")
     document.getElementById("install").style.display = "none"
     if (localStorage["pwa-enabled"]) {
         startPwa();
