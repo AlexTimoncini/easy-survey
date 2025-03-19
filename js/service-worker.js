f38804b71d0bf84cc1e0bf36d362aff7e0045cb9
Poratile_Alex <>
2025-03-19 11:34:07 +0100

    upload

--- js/service-worker.js
+++ js/service-worker.js
@@ -1,50 +1,27 @@
-const APP_PREFIX = 'EasySurvey'
-const VERSION = '1.00'
-const CACHE_NAME = APP_PREFIX + VERSION
-const URLS = [
-    '/easy-survey/assets/css/general.css',
-    '/easy-survey/assets/css/navbar.css',
-    '/easy-survey/assets/css/loader.css',
-    '/easy-survey/assets/css/footer.css',
-    '/easy-survey/index.html'
-]
+self.addEventListener("install", function(event) {
+    event.waitUntil(
+        caches.open("pwa").then(function(cache) {
+            return cache.addAll([
+                '/easy-survey/assets/css/general.css',
+                '/easy-survey/assets/css/navbar.css',
+                '/easy-survey/assets/css/loader.css',
+                '/easy-survey/assets/css/footer.css',
+                '/easy-survey/index.html'
+            ]);
+        })
+    );
+});
 
-// Respond with cached resources
-self.addEventListener('fetch', function (e) {
-    console.log('fetch request : ' + e.request.url)
-    e.respondWith(
-        caches.match(e.request).then(function (request) {
-            if (request) { // if cache is available, respond with cache
-                console.log('responding with cache : ' + e.request.url)
-                return request
-            } else {       // if there are no cache, try fetching request
-                console.log('file is not cached, fetching : ' + e.request.url)
-                return fetch(e.request)
-            }
-        })
-    )
-})
-self.addEventListener('install', function (e) {
-    e.waitUntil(
-        caches.open(CACHE_NAME).then(function (cache) {
-            console.log('installing cache : ' + CACHE_NAME)
-            return cache.addAll(URLS)
-        })
-    )
-})
-self.addEventListener('activate', function (e) {
-    e.waitUntil(
-        caches.keys().then(function (keyList) {
-            let cacheWhitelist = keyList.filter(function (key) {
-                return key.indexOf(APP_PREFIX)
-            })
-            cacheWhitelist.push(CACHE_NAME)
-            return Promise.all(keyList.map(function (key, i) {
-                if (cacheWhitelist.indexOf(key) === -1) {
-                    console.log('deleting cache : ' + keyList[i] )
-                    return caches.delete(keyList[i])
+self.addEventListener("fetch", function(event) {
+    event.respondWith(
+        caches.open("pwa").then(function(cache) {
+            return cache.match(event.request).then(function(response) {
+                cache.addAll([event.request.url]);
+                if (response) {
+                    return response;
                 }
-            }))
+                return fetch(event.request);
+            });
         })
-    )
-})
+    );
+});
