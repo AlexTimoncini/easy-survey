8de8ea66be5badbfc213e651d4171680270dc09a
Poratile_Alex <>
2025-03-19 15:06:24 +0100

    upload

--- js/welcome.js
+++ js/welcome.js
@@ -1,6 +1,10 @@
 init()
 function init() {
-    document.getElementById("download").addEventListener("click", ()=>{
-        document.getElementById("install").click()
-    })
+    if (window.matchMedia("(display-mode: standalone)").matches || window.navigator.standalone) {
+        document.getElementById("download").addEventListener("click", ()=>{
+            document.getElementById("install").click()
+        })
+    } else {
+        document.getElementById("download").remove()
+    }
 }
